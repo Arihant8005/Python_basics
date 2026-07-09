5407d3c17b5fe10df9a841433460caf9653a1713
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-09 22:14:28 +0530

    How to take input in python

---
+++ Input_taking_in_python.py
@@ -0,0 +1,5 @@
+#input taking from user and printing it
+name=input(("name:"))
+age=int(input("age:"))
+price=float(input("price:"))
+print("my name is",name,"my age is",age,"i hanve bought a new car whose price is",price,"rupees")
