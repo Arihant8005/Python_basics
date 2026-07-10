ad4f78ae7edf4a25880d6e6a0622ef64a2131788
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-10 19:01:31 +0530

    Use of conditional statements

---
+++ conditional_statement.py
@@ -0,0 +1,10 @@
+#traffic light code
+light=input("light:")
+if(light=="green"):
+    print("go")
+elif(light=="yellow"):
+    print("look")
+elif(light=="red"):
+    print("stop")
+else:
+    print("invalid input")
