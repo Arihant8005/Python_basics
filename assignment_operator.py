7d651b421145373e951709bca127cb4eca9fe291
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-10 18:56:11 +0530

    Use of assignment operators

---
+++ assignment_operator.py
@@ -0,0 +1,19 @@
+num1 = int(input("num1: "))
+num1 += 10
+print(num1)
+
+num2 = int(input("num2: "))
+num2 = num2**2    #power operator
+print(num2)
+
+num3 = int(input("num3: "))
+num3 -= 10
+print(num3)
+
+num4 = int(input("num4: "))
+num4 *= 2
+print(num4)
+
+num4 = int(input("num4: "))
+num4 /= 5
+print(num4)
