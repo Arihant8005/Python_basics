607d539a3076606eff711ae07e396d3a73e66ad1
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-10 19:24:02 +0530

    Finding simple_interest

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+P = int(input("Enter the amount: "))
+R = int(input("Enter the rate of interest: "))
+T = int(input("Enter the time period: "))
+
+simple_interest = (P * R * T) / 100
+print(simple_interest)
