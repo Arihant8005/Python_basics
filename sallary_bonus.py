368aff4b6c56e47f69e47974880cde40678edce0
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-11 11:54:20 +0530

    Adding sallary bonus problem

--- sallary_bonus.py
+++ sallary_bonus.py
@@ -1,3 +1,5 @@
+# input sallary from user and if salary is greater than 5000 than give 5% of his sallary as bonus otherwise give bonus = 250 rupees
+
 num=int(input("salary:"))
 if(num>=5000):
     bonus=0.05*num
