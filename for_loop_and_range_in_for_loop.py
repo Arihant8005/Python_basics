5c9823d3c1d5fba61be3008ff73ef2fc7b373d00
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-13 21:44:45 +0530

    Added syntax of for loop

---
+++ for_loop_and_range_in_for_loop.py
@@ -0,0 +1,10 @@
+kuchbi=[1,23,45,"arihamt","potato","mango"]   #creating a list
+for el in kuchbi:
+    print(el)             #el=element
+print(kuchbi[2])
+
+for i in range(1,11):     # syntax:
+    print(i)                  # for el in range(start,stop,step) [here "stop" element is not printed]
+
+for i in range(1,11,2):
+    print(i)
