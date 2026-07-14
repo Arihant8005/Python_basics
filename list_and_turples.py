044a618e2662be355cca73cb3668aef3d3860389
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-14 21:04:58 +0530

    Basics of list and turples

---
+++ list_and_turples.py
@@ -0,0 +1,12 @@
+list = [12,"arihant","age",17,"poornima"]
+print(list)
+print(len(list))
+print(list[1])#index in list
+
+tup=(12,"arihant","age",17,"poornima")
+print(tup)
+print(len(tup))
+print(tup[1])
+
+# tup[1]="rathore" not support this
+# both are same onlydifferenceis of[],()
