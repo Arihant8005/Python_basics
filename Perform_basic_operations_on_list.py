e3af1d20bbc61e3dbcf744366ada8da0146003cc
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-15 09:24:36 +0530

    Adding basic operations on list

---
+++ Perform_basic_operations_on_list.py
@@ -0,0 +1,24 @@
+# write a program to perform  the following operations: create , access , update and  delete
+
+list = [11 , "Arihant", 12.89 , 0]   # Createing list
+print(list)
+print(list[1])             # access list
+list[1] = "Rathore"       # update list
+print(list)
+list.remove(0)           # delete list
+list.remove("Rathore")
+print(list)
+
+#  OR
+# list_1 = []                       # creating list
+
+# list_1.append("12")
+# list_1.append("arihant")
+# list_1.append("12.99")
+# print(list_1)
+# print(list_1[2])           # access list
+# list_1[2] = "999"      # update list
+# print(list_1)
+# list_1.remove("arihant")
+# list_1.remove("12")
+# print(list_1)
