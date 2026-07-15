7ed32ff3f7e632eeadca36f13ba121ba859f60b2
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-15 08:34:49 +0530

    Added Temperature_conversion.py

---
+++ Temperature_conversion.py
@@ -0,0 +1,8 @@
+#  write a python program that asks the user for a temperature in celsius and
+#  converts and output the temperature in fahrenheit
+
+temp_cel = int(input("Enter the temperature in celsius : "))
+
+temp_fah = temp_cel + 273
+
+print(temp_fah)
