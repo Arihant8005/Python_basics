1cd7043e3fb7330baf37274b9d54bd086e5ad7bd
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-15 08:20:25 +0530

    Prime number

---
+++ prime_number.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number: "))
+
+count = 0
+for i in range(1 , n + 1):
+    if(n % i == 0):
+        count +=1
+
+if(count == 2):
+    print("It's a prime number")
+else:
+    print("Not a prime number")
