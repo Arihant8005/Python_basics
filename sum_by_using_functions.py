2a7fcae83bd260611e73905f154e65373b4a0427
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-15 09:31:11 +0530

    Sum by functions

---
+++ sum_by_using_functions.py
@@ -0,0 +1,13 @@
+def cal_sum(a,b):
+    sum=a+b
+    print(sum)
+    return sum
+
+cal_sum(4,5)
+#more lines of code
+#more lines of code
+cal_sum(2,5)
+#more lines of code
+#more lines of code
+cal_sum(44,2)
+#more lines of code
