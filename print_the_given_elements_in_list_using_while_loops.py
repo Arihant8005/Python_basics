6ae6e8322433991ba303ab5f20f345f796a22508
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-16 21:30:43 +0530

    Printing given elements using while loop

---
+++ print_the_given_elements_in_list_using_while_loops.py
@@ -0,0 +1,13 @@
+nums=[1,4,9,16,25,36,49,64,81,100]
+idx=0
+while idx<=9:                             #printing all elements of a list using index
+    print(nums[idx])
+    idx+=1
+
+nums=[1,4,9,16,25,36,49,64,81,100]
+x=36
+idx=0
+while idx<=9:
+    if(nums[idx]==x):
+        print("found at index",idx)
+    idx+=1
