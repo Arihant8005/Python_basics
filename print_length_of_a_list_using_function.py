57fbcfee2ea9e8778417e9ba906294c7b17d4760
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-16 21:07:07 +0530

    printing length of a list using function

---
+++ print_length_of_a_list_using_function.py
@@ -0,0 +1,10 @@
+list1=["arihant","rathore",12,19,"jaipur"]
+list2=["gopi","potato",12.43,"kishan"]
+
+def print_len(list):
+    print(len(list))
+
+print(len(list1))
+#more lines of code
+print(len(list2))
+#more lines of code
