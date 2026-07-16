f7a42f4ae95d04669fefdbed51a53917c462eb4a
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-16 20:50:01 +0530

    Adding string_indexing_length_and cutting

---
+++ string_indexing_length_cutting.py
@@ -0,0 +1,22 @@
+str="arihant"
+
+print(len(str))
+print(str[2])
+
+str1="rathore"
+
+print(len(str1))
+print(str1[2])
+
+final_string = str + " " + str1
+
+print(final_string)
+#or print(str+ " " +str1)
+print(len(final_string))  #length of space is also counted
+print(final_string[2])
+
+str2="arihant rathore"
+
+print(str2[:8])   #cutting
+print(str2[0:])
+print(str2[1:5])
