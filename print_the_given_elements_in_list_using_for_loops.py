a4a173908939ae9dcbd007972ffd70c6b67de9af
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-16 21:29:41 +0530

    Printing element of a list using for loop

---
+++ print_the_given_elements_in_list_using_for_loops.py
@@ -0,0 +1,10 @@
+nums=[1,4,9,16,25,36,49,64,81,100]
+for idx in range(0 , 10):
+    print(nums[idx])
+
+
+nums=[1,4,9,16,25,36,49,64,81,100]
+x = 100
+for idx in range(0 , 10):
+    if(nums[idx] == x):
+        print("Found at index : ", idx)
