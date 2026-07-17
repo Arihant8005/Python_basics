fd2404c0bffe21016a3906e04daa797a31cbf238
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-17 08:24:51 +0530

    Adding distrionary and sets

---
+++ distrionary_and_set.py
@@ -0,0 +1,17 @@
+dis = {
+    "key":"value",
+    "name":"arihant",
+    "age":19,
+    "place":"jaipur",
+  "marks":92.8
+}
+print(dis)
+
+set = {12,34,"arihant","rathore","jaipur"}
+print(set)
+print(type(set))
+print(len(set))
+
+set1 = {2,2,2,3,"arihant","arihant"}
+print(set1)                              #repeted num. are printed only one time
+print(len(set1))
