3d48970ca5bf7825902f723a551e38fbbf6e3701
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-17 08:27:45 +0530

    Added class and object

---
+++ class_and_object_in_phython.py
@@ -0,0 +1,16 @@
+class Student:          #creating class
+    name = "arihant"
+    marks = 92.8
+
+s1 = Student()           #creating object(instance)
+print(s1.name,s1.marks)
+
+
+class Car:
+    name="alto"
+    colour="blue"
+
+c1=Car()
+print(c1.name,c1.colour)
+c2=Car()               #same name and colour will be stored
+print(c2.name,c2.colour)
