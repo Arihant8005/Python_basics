ac46de842b427fa6b9f9aa1f82605c49d561c927
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-18 10:08:24 +0530

    Added multi level inheritance

---
+++ inheritance_in_phython_2.py
@@ -0,0 +1,18 @@
+#multi level inhetrance
+class car:
+    def start(self):
+        print("starting..")
+    def stop(self):
+        print("stoping..")
+class Toyota(car):
+    def __init__(self,name):
+        self.name=name
+class Alto(Toyota):
+    def __init__(self,brand):
+        self.brand=brand
+
+c1=Alto("kishan")
+c2=Toyota("gopi")
+print(c2.name)
+print(c1.brand)
+c1.start()
