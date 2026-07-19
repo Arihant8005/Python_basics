17410dcaaa4e7fa6ae1b6e758ee4d7780aeb6183
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-19 17:12:29 +0530

    Added type_conservation

---
+++ type_conservation.py
@@ -0,0 +1,14 @@
+a="31.2"
+print(type(a))
+
+a="31.2"                #we can conver this string into float
+b=float(a)
+print(type(b))
+
+a=31.2                #we can conver this integer value into string
+b=str(a)
+print(type(b))
+
+a="34"                 #we can conver this string into integer 
+b=int(a)
+print(type(b))
