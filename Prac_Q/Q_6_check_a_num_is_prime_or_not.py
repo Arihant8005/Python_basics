d5502893fc18c4d7e8cf7f622cc1f246eed16793
Arihant8005 <arihantrathore8005@gmail.com>
2026-07-21 08:24:16 +0530

    checking a number is prime or not

---
+++ Prac_Q/Q_6_check_a_num_is_prime_or_not.py
@@ -0,0 +1,16 @@
+# 6. Check for Prime Number
+
+# Input a number and determine whether it’s prime.
+# Concepts: loops, divisibility, early exit logic
+
+num = int(input("Enter a number:"))
+count=0
+
+for i in range(1,num+1):
+    if(num%i==0):
+        count+=1
+
+if(count==2):
+    print("prime number")
+else:
+    print("not a prime number")
